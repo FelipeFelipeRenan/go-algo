4b38c070a1e0288e88d96fdbdbf0194116744bb7
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-28 17:16:15 -0300

    fix: fixing sum of evens

--- arrays/easy/sum_of_evens/sum_of_evens.go
+++ arrays/easy/sum_of_evens/sum_of_evens.go
@@ -2,13 +2,11 @@ package main
 
 func SumOfEvens(num []int) int {
 
-	numeros := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
 	acc := 0
-	for _, v := range numeros {
+	for _, v := range num {
 		if v%2 == 0 {
 			acc = acc + v
 		}
 	}
-	return (acc)
+	return acc
 }
