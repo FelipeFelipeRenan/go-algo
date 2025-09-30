c72c6c9b5d2cf02746376f3a4fd4f205b6c698c6
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-09-30 14:14:59 -0300

    feat: adding minumum sum subarray solution

--- sliding-window/minimum-size-subarray-sum/minimum_size_subarray_sum.go
+++ sliding-window/minimum-size-subarray-sum/minimum_size_subarray_sum.go
@@ -1,31 +1,33 @@
 package main
 
+import "math"
+
 func MinSubArrayLen(target int, nums []int) int {
 
 	if len(nums) == 0 {
 		return 0
 	}
 
+	left := 0
 
-
-	size := 0
-
-	left, right := 0, 0
-
+	minLenght := math.MaxInt
 	k := 0
 
-	for right < len(nums) {
+	for right := range(nums) {
 		k += nums[right]
 
-		if k > target{
-			size = 0
+		for k >= target {
+			if right-left+1 < minLenght {
+				minLenght = right - left + 1
+			}
+			k -= nums[left]
 			left++
 		}
-		size++
-		right++
 	}
 
-
-	return size
+	if minLenght == math.MaxInt{
+		minLenght = 0
+	}
+	return minLenght
 
 }
