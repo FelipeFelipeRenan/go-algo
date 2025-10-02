29158c0442d01b9e7d620ae9e84f617c47b32a2b
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-10-02 15:53:40 -0300

    feat: adding binary search solving

--- binary-search/classic/binary_search.go
+++ binary-search/classic/binary_search.go
@@ -8,6 +8,7 @@ func Search(nums []int, target int) int{
 	meio := left + (right - left) / 2
 
 	for left <= right{
+		meio = left + (right - left) / 2
 		if nums[meio] == target {
 			return meio
 		}
@@ -17,7 +18,6 @@ func Search(nums []int, target int) int{
 		if nums[meio] < target {
 			left = meio + 1
 		}
-		meio = left + (right - left) / 2
 	}
 	return -1
 }
